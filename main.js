c96cbe41d50b97d4ad06624de41bfa67ce154a6f
OkamotoAiri <airin0322@gmail.com>
2022-03-11 17:18:13 +0900

    errorMsg

--- main.js
+++ main.js
@@ -1,49 +1,67 @@
-
-(function () {
+(function() {
 	'use strict'; {
 	  let app = new Vue({
 		el: '#app',
-		newItem: '',
 		data: {
-		  todos: []
+		  todos: [],
+		  newItem: '',
+		  isChecked: true
 		},
 		watch: {
 		  todos: {
-			handler: function () {
+			handler: function() {
 			  localStorage.setItem('todos', JSON.stringify(this.todos));
 			},
 			deep: true
 		  }
 		},
-		mounted: function () {
+		mounted: function() {
 		  this.todos = JSON.parse(localStorage.getItem('todos')) || [];
 		},
 		methods: {
-		  addItem: function () {
+		  addItem: function() {
+			if (this.newItem == '') {
+			  $('.error').show();
+			  return;
+			}
 			let item = {
 			  title: this.newItem,
 			  isDone: false,
 			}
 			this.todos.push(item);
 			this.newItem = '';
+			$('.error').hide();
+  
 		  },
-		  deleteItem: function () {
+		  deleteItem: function() {
 			if (!confirm('チェックした項目を削除しますか？')) {
 			  return;
 			}
 			this.todos = this.remaining;
 		  },
-		  editItem: function (index) {
+		  ditItem: function(index) {
 			$('.update').eq(index).toggle();
 		  }
 		},
 		computed: {
-		  remaining: function () {
-			return this.todos.filter(function (todo) {
+		  remaining: function() {
+			return this.todos.filter(function(todo) {
 			  return !todo.isDone;
 			});
-		  }
+		  },
 		}
 	  });
 	}
+  
   })();
+  
+  
+  // $('#checkAll').on('click',function(){
+  
+  // 	$('.checks').prop('checked',this.checked);
+  // 	if ($('.checks').hasClass('done')){
+  // 	}else{
+  // 		$('.checks').addClass('done');
+  // 	}
+  
+  // });
